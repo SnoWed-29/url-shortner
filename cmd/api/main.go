e6ce75ee9ddb84ed6abdb2c6b7c6774f142a37a6
snowed-29 <snowed29@gmail.com>
2026-09-17 22:20:57 +0100

    feat: connect api to postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"context"
+	"time"
+	"github.com/SnoWed-29/url-shortener/internal"
 )
 
 func main() {
+
+	config := internal.LoadConfig()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	db, err := internal.NewPostgresPool(ctx, config.PostgresDSN)
+
+	if err != nil {
+		log.Fatalf("database connection failed: %v", err)
+	}
+	defer db.Close()
+
+	log.Println("connected to PostgreSQL")
+	
+
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
+		defer cancel()
+
+		if err := db.Ping(ctx); err != nil {
+			http.Error(w, "database unavailable", http.StatusServiceUnavailable)
+			return 
+		}
+
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "ok")
 	})
 
-	addr := ":8080"
+	addr := ":" + config.AppPort
+
 	
 	log.Printf("API Server Listening on %s", addr)
 
